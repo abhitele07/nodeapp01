5d340c8eafc5145578d690b6fe8f5fcbe8cbf797
abhitele07 <abhitele4151@gmail.com>
2023-07-17 15:14:29 +0000

    end wala

---
+++ index.js
@@ -0,0 +1,24 @@
+//importing node framework
+
+var express = require(‘express’);
+ 
+
+var app = express();
+
+//Respond with "hello world" for requests that hit our root "/"
+
+app.get(‘/’, function (req, res) {
+
+res.send(‘hello shiv i love you too’);
+
+});
+
+//listen to port 80 by default
+
+app.listen(process.env.PORT || 80);
+ 
+
+module.exports = app;
+ 
+
+execute shell- npm install
